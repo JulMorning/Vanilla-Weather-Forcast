7c3f9cb07b394849a22251b477033a3e8d1693f9
Yuliia <106186932+JulMorning@users.noreply.github.com>
2022-11-19 15:25:05 +0200

    Current Weather Condition added / JS

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function displayWeatherCondition(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "5dd071644aff4379355022a20839a99e";
+let city = "Kyiv";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayWeatherCondition);
