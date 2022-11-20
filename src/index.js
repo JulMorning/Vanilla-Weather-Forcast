31390e86d73d6982dcdeb2343079d841f8239f9a
Yuliia <106186932+JulMorning@users.noreply.github.com>
2022-11-20 13:30:25 +0200

    Search form added / HTML & JS

--- src/index.js
+++ src/index.js
@@ -41,8 +41,19 @@ function displayWeatherCondition(response) {
   );
   weatherIconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "5dd071644aff4379355022a20839a99e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
 
-let apiKey = "5dd071644aff4379355022a20839a99e";
-let city = "Lisbon";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayWeatherCondition);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Kyiv");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
